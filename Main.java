201e0744ca7b2d093b1874c04324b284e18b498b
Chaostheory05 <luisbancesestrada@gmail.com>
2025-01-22 09:09:22 -0600

    Se agregó loop al menú

--- Main.java
+++ Main.java
@@ -1,19 +1,34 @@
 import java.util.Scanner;
 public class Main {
+
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Lavadora lavadora = new Lavadora();
-        System.out.println("Elige las siguientes opciones:");
-        System.out.println("1.Encender");
-        System.out.println("2.Ajustar velocidad");
-        String opcion = input.nextLine();
+        boolean i = true;
 
-        if (opcion.equals("1")) {
-            lavadora.encender();
-        }if (opcion.equals("2")) {
-            lavadora.aumentarVelocidad();
+            Scanner input = new Scanner(System.in);
+            Lavadora lavadora = new Lavadora();
 
-        }
+
+            do {
+                System.out.println("Elige las siguientes opciones:");
+                System.out.println("1.Encender");
+                System.out.println("2.Ajustar velocidad");
+                System.out.println("3.Terminar programa");
+                String opcion = input.nextLine();
+
+                if (opcion.equals("1")) {
+                    lavadora.encender();
+                }if (opcion.equals("2")) {
+                    lavadora.aumentarVelocidad();
+                }
+                if (opcion.equals("3")) {
+                    i = false;
+                }
+            }while(i == true);
+
+
+
+
 
 
 
